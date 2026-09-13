1e88f35f6c4cba7da34cd1436c2ab81348efab4e
Vanshika-182 <vanshikakesarwani182@gmail.com>
2026-09-13 12:40:52 +0530

    Add start  and game screens

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Fruit Cutter game Loaded!");
+const startButton = document.getElementById("startButton");
+const startScreen = document.getElementById("startScreen");
+const gameScreen = document.getElementById("gameScreen");
+
+startButton.addEventListener("click", function () {
+    startScreen.style.display = "none";
+    gameScreen.style.display = "block";
+});
